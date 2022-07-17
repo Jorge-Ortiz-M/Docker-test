e0dca5914c3103558bcc4582dd4508bdfc0398fe
Jorge-Ortiz-M <jorge.ortiz@icalialabs.com>
2022-07-16 23:46:09 -0500

    Adding Post model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+    
+    validate :name_with_jorge
+
+    def name_with_jorge
+        name === "Jorge" ? errors.add(:name, "Should not be Jorge") : nil
+    end
 end
